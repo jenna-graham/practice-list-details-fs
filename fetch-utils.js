0cfafa1cdebbd88aa1b3608e6834585c4d8e5b72
jenna-graham <jclayto2@gmail.com>
2022-05-13 15:55:20 -0700

    added supabase to html and js, created get dragons function

---
+++ fetch-utils.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = 'https://icyrbwltlybmigeljxmh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImljeXJid2x0bHlibWlnZWxqeG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMDMxMzMsImV4cCI6MTk2Nzg3OTEzM30.kUzjWO4wi0A9AVcWdFt_BG9uq-HBZoAR2aZ4IRN55yw';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getDragons() {
+    const resp = await client.from('dragons').select('*'); 
+
+
+}
